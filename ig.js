8c4c43c6477c9a1a9844f5922fcf96fb323cc77b
TdrckAlfa <75511385+Alphakingg-ai@users.noreply.github.com>
2022-07-13 21:42:57 +0700

    Update ig.js

--- ig.js
+++ ig.js
@@ -10,13 +10,12 @@ bot.on(/^\/ig ([\s\S]+)/, async (msg, args) => {
     bot.sendMessage(msg.chat.id, "Silahkan tunggu beberapa saat, proses membutuhkan waktu beberapa menit")
     const arg = args.match[1]
     res = await cal.downloader.instagram.post(arg)
-    console.log(res)
     capt = "Berhasil mengunduh dari instagram"
      for (let i of res.medias) {
-     if (i.type == "video") {
+     if (i.type.includes("video")) {
      bot.sendVideo(msg.chat.id, i.url, {caption: capt})
      } 
-     if (i.type == "image") {
+     if (i.type.includes("image")) {
      bot.sendPhoto(msg.chat.id, i.url, {caption: capt})
       } else {
       bot.sendMessage(msg.chat.id, "Unexpected Error 605")
