4fb2cd00f42072f2ab54c298d0ed12e0b1278ce4
TdrckAlfa <75511385+Alphakingg-ai@users.noreply.github.com>
2022-07-13 22:01:53 +0700

    Update ig.js

--- ig.js
+++ ig.js
@@ -11,12 +11,16 @@ bot.on(/^\/ig ([\s\S]+)/, async (msg, args) => {
     const arg = args.match[1]
     res = await cal.downloader.instagram.post(arg)
     capt = "Berhasil mengunduh dari instagram"
+    try {
      for (let i of res.medias) {
-     if (i.type.includes("video")) {
+     if (i.type === "video") {
      bot.sendVideo(msg.chat.id, i.url, {caption: capt})
-     } else if (i.type.includes("image")) {
+     } else if (i.type === "image") {
      bot.sendPhoto(msg.chat.id, i.url, {caption: capt})
       }
+} catch (e) {
+console.log("Err: "+e)
+}
      }
    })
   }
